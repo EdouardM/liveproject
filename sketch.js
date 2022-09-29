aa7d0c534f0e41a68ecdf9dbf2c69a75ccb4335a
Edouard Moureaux <edouard.moureaux@gmail.com>
2022-09-29 07:14:11 +0200

    Initializd project

---
+++ sketch.js
@@ -0,0 +1,20 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  // couleur fond
+  background(220);
+
+  // remplissage
+  fill(100, 0, 100);
+  
+  // contour
+  stroke(0,0, 0);
+
+  // trait
+  line(12,12,20,3);
+
+  // rectangle
+  rect(100, 102,120, 20);
+}
